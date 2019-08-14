34138a026526da3c682ea0611a9ec135c97329f3
benjaminjb <benjamin.blattberg@gmail.com>
2019-08-14 15:50:13 -0500

    print whole clone_member

--- patroni/ha.py
+++ patroni/ha.py
@@ -217,24 +217,26 @@ class Ha(object):
 
     def clone(self, clone_member=None, msg='(without leader)'):
         logger.warn("HELLO CLONE FUNC")
+        logger.warn("HELLO msg=", msg)
         if clone_member:
             logger.warn("HELLO CLONE MEMBER")
         if self.is_standby_cluster() and not isinstance(clone_member, RemoteMember):
             logger.warn("is standby cluster and not isinstance(clone_member, RemoteMember)")
             clone_member = self.get_remote_member(clone_member)
 
-        logger.warn("HELLO primary_slot_name")
-        logger.warn(str(clone_member.primary_slot_name))
-        logger.warn("HELLO create_replica_methods")
-        logger.warn(str(clone_member.create_replica_methods))
-        logger.warn("HELLO index")
-        logger.warn(str(clone_member.index))
-        logger.warn("HELLO name")
-        logger.warn(str(clone_member.name))
-        logger.warn("HELLO session")
-        logger.warn(str(clone_member.session))
-        logger.warn("HELLO data")
-        logger.warn(str(clone_member.data))
+        logger.warn(str(clone_member))
+        # logger.warn("HELLO primary_slot_name")
+        # logger.warn(str(clone_member.primary_slot_name))
+        # logger.warn("HELLO create_replica_methods")
+        # logger.warn(str(clone_member.create_replica_methods))
+        # logger.warn("HELLO index")
+        # logger.warn(str(clone_member.index))
+        # logger.warn("HELLO name")
+        # logger.warn(str(clone_member.name))
+        # logger.warn("HELLO session")
+        # logger.warn(str(clone_member.session))
+        # logger.warn("HELLO data")
+        # logger.warn(str(clone_member.data))
 
         self._rewind.reset_state()
         if self.state_handler.bootstrap.clone(clone_member):
